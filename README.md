29aabc9d88d7d57b98820f3fd8fcfb7b5c9b7e92
Steve Winton <stevewinton@gmail.com>
2019-02-25 18:15:51 -0600

    Proof of concept

---
+++ README.md
@@ -0,0 +1,23 @@
+# `octokit-routes-insomnia`
+
+:soon:
+
+## Datatypes
+
+### Request
+
+```json
+{
+  "parentId": "__WORKSPACE_ID__",
+  "_id": "__REQ_1__",
+  "_type": "request",
+  "name": "Get a repo",
+  "description": "",
+  "headers": [],
+  "authentication": {},
+  "method": "GET",
+  "url": "{{ github_api_root  }}/repos/atom/atom",
+  "body": {},
+  "parameters": []
+}
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const routes = require('@octokit/routes/routes/api.github.com/repos');
+
+const normalizePath = path => {
+  // Replace paths like /repos/:owner/:repo with /repos/{{ owner }}/{{ repo }}
+  const re = /:([^/]+)/g;
+  return path.replace(re, (match, captured) => {
+    return `{{ ${captured} }}`;
+  });
+};
+
+const resources = routes.map((route, index) => {
+  return {
+    parentId: '__WORKSPACE_ID__',
+    _id: `__REQ_${index}__`,
+    _type: 'request',
+    name: route.name,
+    description: `${route.description}\n\n${route.documentationUrl}`,
+    headers: [],
+    authentication: {},
+    method: route.method,
+    url: `{{ github_api_root  }}${normalizePath(route.path)}`,
+    body: {},
+    parameters: []
+  };
+});
+
+const data = {
+  _type: 'export',
+  __export_format: 3,
+  __export_date: new Date(),
+  __export_source: 'insomnia.importers:v0.1.0',
+  resources
+};
+
+console.log(JSON.stringify(data, null, 2));
