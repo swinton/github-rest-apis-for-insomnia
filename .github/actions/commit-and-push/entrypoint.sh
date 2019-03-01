a9271388208a39daf202d614c041c0346908f693
Steve Winton <stevewinton@gmail.com>
2019-03-01 10:56:19 -0600

    Hard-code commit message into COMMIT_MSG

--- .github/actions/commit-and-push/entrypoint.sh
+++ .github/actions/commit-and-push/entrypoint.sh
@@ -9,5 +9,9 @@ git config user.name "${GITHUB_REPOSITORY}[bot]"
 # Grab granch name
 BRANCH=$( git symbolic-ref --short HEAD )
 
+COMMIT_MSG="build: spec
+
+skip-checks:true"
+
 # Commit and push _just_ the specs
-git commit routes/api.github.com/github-rest-apis-for-insomnia.json -m "build: spec\n\nskip-checks: true" && git push --set-upstream origin "${BRANCH}"
+git commit routes/api.github.com/github-rest-apis-for-insomnia.json -m "${COMMIT_MSG}" && git push --set-upstream origin "${BRANCH}"
