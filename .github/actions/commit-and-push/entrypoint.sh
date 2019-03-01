fb930b2e94aacead560be73c411b3b8bacec550d
Steve Winton <stevewinton@gmail.com>
2019-03-01 08:31:03 -0600

    Get pusher details from push event payload

--- .github/actions/commit-and-push/entrypoint.sh
+++ .github/actions/commit-and-push/entrypoint.sh
@@ -2,4 +2,10 @@
 
 set -e
 
+USER_EMAIL=$( jq --raw-output '.pusher.email' "${GITHUB_EVENT_PATH}" )
+USER_NAME=$( jq --raw-output '.pusher.name' "${GITHUB_EVENT_PATH}" )
+
+git config user.email "${USER_EMAIL}"
+git config user.name "${USER_NAME}"
+
 git commit -am "build: spec"
