e13bfd0fc2d177e9d91d96a0cfc3525fd1e21a7d
Steve Winton <stevewinton@gmail.com>
2019-03-01 10:41:49 -0600

    Re-introduce hard-coded user config

--- .github/actions/commit-and-push/entrypoint.sh
+++ .github/actions/commit-and-push/entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Hard-code user config
+git config user.email "bots@wintron.codes"
+git config user.name "${GITHUB_REPOSITORY}[bot]"
+
+# Grab granch name
 BRANCH=$( git symbolic-ref --short HEAD )
 
-git commit routes/api.github.com/github-rest-apis-for-insomnia.json -m "build: spec" --author="${GITHUB_REPOSITORY}[bot] <bots@wintron.codes>" && git push --set-upstream origin "${BRANCH}"
+# Commit and push _just_ the specs
+git commit routes/api.github.com/github-rest-apis-for-insomnia.json -m "build: spec" && git push --set-upstream origin "${BRANCH}"
