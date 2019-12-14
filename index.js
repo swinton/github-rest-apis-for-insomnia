f146a3b08e91296de529b99c1d0d317a958815f1
Steve Winton <stevewinton@gmail.com>
2019-12-13 20:34:51 -0600

    Re-org

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-const data = require('./generate')();
+const data = require('./lib/generate')();
 
 // Destination for output
 const destination = path.normalize(
