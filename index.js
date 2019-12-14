49af11c8205b7ec1f4bef61fe75518a5ffa37784
Steve Winton <stevewinton@gmail.com>
2019-12-14 12:03:09 -0600

    Generate all REST APIs

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-const api = require('@octokit/routes/api.github.com');
 const meta = require('./package');
-const data = require('./lib/generate')({ api, meta });
+const generate = require('./lib/generate');
 
-// Destination for output
-const destination = path.normalize(
-  path.join(__dirname, 'routes', 'api.github.com', 'github-rest-apis-for-insomnia.json')
-);
+const apis = [
+  ['api.github.com', path.join(__dirname, 'routes', 'api.github.com', 'github-rest-apis-for-insomnia.json')],
+  ['ghe-2.16', path.join(__dirname, 'routes', 'ghe-2.16', 'github-rest-apis-for-insomnia.json')],
+  ['ghe-2.17', path.join(__dirname, 'routes', 'ghe-2.17', 'github-rest-apis-for-insomnia.json')],
+  ['ghe-2.18', path.join(__dirname, 'routes', 'ghe-2.18', 'github-rest-apis-for-insomnia.json')],
+  ['ghe-2.19', path.join(__dirname, 'routes', 'ghe-2.19', 'github-rest-apis-for-insomnia.json')]
+];
 
-// Write output straight to file
-const output = JSON.stringify(data, null, 2);
-fs.writeFileSync(destination, output);
+apis.forEach(([source, dest]) => {
+  // eslint-disable-next-line global-require,import/no-dynamic-require
+  const api = require(`@octokit/routes/${source}`);
+  const data = generate({ api, meta });
+  const destination = path.normalize(dest);
+
+  // Write output straight to file
+  const output = JSON.stringify(data, null, 2);
+  fs.writeFileSync(destination, output);
+});
+
+process.exit(0);
