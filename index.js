1a437108d48ea6d611d91ff08ceb57bfde96c297
Steve Winton <stevewinton@gmail.com>
2019-12-15 10:31:46 -0600

    Dynamically generate all @octokit/routes

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
+const routes = require('@octokit/routes');
 const meta = require('./package');
 const generate = require('./lib/generate');
 
-const apis = [
-  ['api.github.com', path.join(__dirname, 'routes', 'api.github.com', 'github-rest-apis-for-insomnia.json')],
-  ['ghe-2.16', path.join(__dirname, 'routes', 'ghe-2.16', 'github-rest-apis-for-insomnia.json')],
-  ['ghe-2.17', path.join(__dirname, 'routes', 'ghe-2.17', 'github-rest-apis-for-insomnia.json')],
-  ['ghe-2.18', path.join(__dirname, 'routes', 'ghe-2.18', 'github-rest-apis-for-insomnia.json')],
-  ['ghe-2.19', path.join(__dirname, 'routes', 'ghe-2.19', 'github-rest-apis-for-insomnia.json')]
-];
-
-apis.forEach(([source, dest]) => {
-  // eslint-disable-next-line global-require,import/no-dynamic-require
-  const api = require(`@octokit/routes/${source}`);
+Object.entries(routes).forEach(([route, api]) => {
   const data = generate({ api, meta });
-  const destination = path.normalize(dest);
+  const destination = path.normalize(path.join(__dirname, 'routes', `${route}.json`));
 
   // Write output straight to file
   const output = JSON.stringify(data, null, 2);
