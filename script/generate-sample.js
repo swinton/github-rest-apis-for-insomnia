3f6da3b3493d1444229dcf0738ff1b1c189036a4
Steve Winton <stevewinton@gmail.com>
2019-12-15 15:05:15 -0600

    Generate more interesting sample

--- script/generate-sample.js
+++ script/generate-sample.js
@@ -1,10 +1,25 @@
+#!/usr/bin/env node
 // eslint-disable-next-line import/no-extraneous-dependencies
 const importers = require('insomnia-importers');
 
 (async () => {
   // Convert a Curl command
-  const output = await importers.convert('curl -H "Accept: foo" -X POST https://insomnia.rest --data "Cool!"');
+  const output = await importers.convert(
+    `curl --request POST \
+    --url https://api.github.com/orgs/wintron/repos \
+    --header 'authorization: Bearer TOKEN' \
+    --header 'content-type: application/json' \
+    --data '{
+      "name": "demo",
+      "description": "This is a demonstration repo for demonstrations",
+      "homepage": "https://github.com/wintron/demo",
+      "private": false,
+      "has_issues": true,
+      "has_projects": false,
+      "has_wiki": false
+    }'`
+  );
   // Pretty print the result
   // eslint-disable-next-line no-console
-  console.log(JSON.stringify(output, null, 2));
+  console.log(JSON.stringify(output.data, null, 2));
 })();
